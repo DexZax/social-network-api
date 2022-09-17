32766477db5d849495b6f6474a06cc7358f3ed4b
Dexter Zacour <cdzacour@hotmail.com>
2022-09-17 11:49:43 -0400

    user controller and routes done

---
+++ models/thought.js
@@ -0,0 +1,13 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+
+    }
+);
---
+++ models/user.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'You must create a username',
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            required: 'you need to provide an email',
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email']
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+          getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('FriendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+
+module.exports = User;
